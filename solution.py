933c213fa431149ab3638ea000c2fcabfb483c6d
Mayra Pena(Peña) <97967943+mayrapena1324@users.noreply.github.com>
2022-12-28 02:31:14 -0600

    Initial commit

---
+++ solution.py
@@ -0,0 +1,36 @@
+class Solution:
+
+# Day 1 p1
+def runningSum(self, nums: list[int]) -> list[int]:
+    total_list = []
+    for i in range(len(nums) + 1):
+        total = sum(nums[:i + 1])
+        total_list.append(total)
+    return total_list
+
+
+# Day 1 p2 - my code
+def pivotIndex(self, nums: list[int]) -> int:
+    for i in range(len(nums)):
+        if i == 0:
+            first_half = 0
+        else:
+            first_half = sum(nums[:i])
+        second_half = sum(nums[i + 1:])
+        print(f"{first_half} vs {second_half}")
+        if first_half == second_half:
+            return i
+    return -1
+
+# faster code by other user - review
+# at each index, you need to find left sum and right sum
+    # if equivalent, return index
+    # else keep going. if you don't find it return -1
+    # def pivotIndex(self, nums: list[int]) -> int:
+    #     left, right = 0, sum(nums)
+    #     for index, num in enumerate(nums):
+    #         right -= num
+    #         if right == left:
+    #             return index
+    #         left += num
+    #     return -1
