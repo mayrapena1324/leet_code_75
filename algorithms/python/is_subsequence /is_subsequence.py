5ace084296b3474515ec7fa082f747bbe0ec4121
Mayra Pena(Peña) <97967943+mayrapena1324@users.noreply.github.com>
2022-12-29 19:10:50 -0600

    solution for is subsequence

--- algorithms/python/is_subsequence /is_subsequence.py
+++ algorithms/python/is_subsequence /is_subsequence.py
@@ -11,4 +11,16 @@ of "abcde" while "aec" is not). """
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        pass
+        i, j = 0, 0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:  # keeping track if we make it through the string
+                i += 1
+            j += 1  # keep counter for t moving
+        if i == len(s):  # check to see if all letters were found by comparing to len of string
+            print(i)
+            return True
+        else:
+            print(i)
+            return False
+
